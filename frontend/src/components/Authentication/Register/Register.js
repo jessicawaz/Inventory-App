8a7a3dd5302f31ce027ba4c9ac2d166c94f93034
jessicawaz <jesswaz05@gmail.com>
2021-11-10 11:43:15 -0500

    Register UI

--- frontend/src/components/Authentication/Register/Register.js
+++ frontend/src/components/Authentication/Register/Register.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { faEye } from "@fortawesome/free-regular-svg-icons";
 import "../Register/Register.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
 
   const registerHandler = (e) => {
     e.preventDefault();
@@ -29,25 +34,52 @@ const Register = () => {
           placeholder="Username"
           onChange={(e) => setName(e.target.value)}
         />
-
         <label htmlFor="email">Email</label>
         <input
-          type="text"
+          type="email"
           name="email"
           value={email}
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <label htmlFor="password">Password</label>
-        <input
-          type="text"
-          name="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={registerHandler}>Register</button>
+        {show ? (
+          <div>
+            <input
+              type="text"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <FontAwesomeIcon
+              className="pass"
+              icon={faEye}
+              onClick={() => setShow(!show)}
+            />
+          </div>
+        ) : (
+          <div>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <FontAwesomeIcon
+              className="pass"
+              icon={faEyeSlash}
+              onClick={() => setShow(!show)}
+            />
+          </div>
+        )}
+
+        <button className="btn-primary" onClick={registerHandler}>
+          Register
+        </button>
       </form>
     </div>
   );
